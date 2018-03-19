fa4216c7696989439c76e800527c2100da9b7095
pocari <caffelattenonsugar@gmail.com>
2018-03-19 14:43:26 +0900

    jsのfunctionのテスト

--- sample.rb
+++ sample.rb
@@ -25,14 +25,17 @@ with_chrome do |driver|
   url = 'https://sikmi.com'
   driver.get url
   # require 'pry-byebug'; binding.pry
-  name = driver.execute_script(<<~EOS)
-    return document.getElementsByTagName('title')[0].text
+  title = driver.execute_script(<<~EOS)
+    function js_function_test(str) {
+      return str + " added by function";
+    }
+    return js_function_test(document.getElementsByTagName('title')[0].text);
   EOS
-  puts "'#{url}' title is '#{name}'"
+  puts "'#{url}' title is '#{title}'"
   width  = driver.execute_script("return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);")
   height = driver.execute_script("return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);")
 
   puts "height: #{height}, width: #{width}"
   driver.manage.window.resize_to(width+100, height+100)
-  driver.save_screenshot('/var/ruby/hoge.png')
+  driver.save_screenshot('/var/ruby/screenshot.png')
 end
